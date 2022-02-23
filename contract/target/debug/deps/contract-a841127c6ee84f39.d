c1b541dae15878d4ac96c0814cbb60999c5fd292
Minjung Sung <minjungsung1994@gmail.com>
2022-02-23 18:22:21 +0900

    reform-2

--- contract/target/debug/deps/contract-a841127c6ee84f39.d
+++
@@ -1,5 +0,0 @@
-/Users/I531482/Projects/personal/rust/nft-tutorial-frontend/contract/target/debug/deps/contract-a841127c6ee84f39: src/main.rs
-
-/Users/I531482/Projects/personal/rust/nft-tutorial-frontend/contract/target/debug/deps/contract-a841127c6ee84f39.d: src/main.rs
-
-src/main.rs:
